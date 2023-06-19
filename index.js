c258bb00737f6d30c44cc13675b7c8b0f2dbc891
pallab-nandi <pallabnandi@outlook.in>
2023-06-19 12:17:30 +0530

    app (/) commands listener added

--- index.js
+++ index.js
@@ -34,5 +34,27 @@ client.once(Events.ClientReady, bot => {
   console.log(`Bot is Ready! Logged in as ${bot.user.tag}...`);
 })
 
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.log(`No command matching ${interaction.commandName} was found!`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (err) {
+    console.log(err);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+    } else {
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+  }
+})
+
 
 client.login(token);
