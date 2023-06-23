af331b35f650c6885a69bcecf370c5fbd63afacf
pallab-nandi <pallabnandi@outlook.in>
2023-06-23 16:21:47 +0530

    config settings added

---
+++ commands/moderations/setting.js
@@ -0,0 +1,149 @@
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder, ChannelType } = require('discord.js');
+const { db } = require('../../db/db.model');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('set')
+    .setDescription('Set your configuration for the server.')
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('welcome_channel')
+        .setDescription('Set your welcome message default channel')
+        .addChannelOption(channel =>
+          channel
+            .setName('target')
+            .setDescription('Enter Channel you want to set as welcome channel')
+            .addChannelTypes(ChannelType.GuildText)
+            .setRequired(true)
+        )
+    )
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('notification_channel')
+        .setDescription('Set your notification message default channel')
+        .addChannelOption(channel =>
+          channel
+            .setName('target')
+            .setDescription('Enter Channel you want to set as notification channel')
+            .addChannelTypes(ChannelType.GuildText)
+            .setRequired(true)
+        )
+    )
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('video_channel')
+        .setDescription('Set your camera only default channel')
+        .addChannelOption(channel =>
+          channel
+            .setName('target')
+            .setDescription('Enter Channel you want to set as video channel')
+            .addChannelTypes(ChannelType.GuildVoice)
+            .setRequired(true)
+        )
+    )
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('grace_period')
+        .setDescription('Set your camera only default channel grace period.')
+        .addStringOption(option =>
+          option
+            .setName('duration')
+            .setDescription('Enter duration for the grace period.')
+            .addChoices( //values are in seconds
+              { name: '15s', value: '15' },
+              { name: '1m', value: '60' },
+              { name: '5m', value: '300' },
+              { name: '15m', value: '900' },
+              { name: '30m', value: '1800' },
+              { name: '1h', value: '3600' },
+              { name: '4h', value: '14400' }
+            )
+            .setRequired(true)
+        )
+    )
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('auto_role')
+        .setDescription('Set a role that will automatically set to new member')
+        .addRoleOption(role =>
+          role
+            .setName('target')
+            .setDescription('Enter auto setup role.')
+            .setRequired(true)
+        )
+    )
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('mod_role')
+        .setDescription('Set a default moderator role.')
+        .addRoleOption(role =>
+          role
+            .setName('target')
+            .setDescription('Enter moderator role.')
+            .setRequired(true)
+        )
+    )
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('ban_role')
+        .setDescription('Set a default ban role')
+        .addRoleOption(role =>
+          role
+            .setName('target')
+            .setDescription('Enter ban role.')
+            .setRequired(true)
+        )
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+  execute: async (interaction) => {
+
+    const guildID = interaction.guildId; //getting guild id
+    const guild = await db.guild.findOne({ guildID }); //getting guild details
+
+    //welcome channel setup
+    if (interaction.options.getSubcommand() === 'welcome_channel') {
+
+      const channel = interaction.options.getChannel('target');
+      await db.guild.findOneAndUpdate({ guildID }, { welcomeChannel: `${channel}` });
+      await interaction.reply(`:white_check_mark: Welcome channel set to ${channel} successfully!`);
+
+    } else if (interaction.options.getSubcommand() === 'notification_channel') {
+
+      const channel = interaction.options.getChannel('target');
+      await db.guild.findOneAndUpdate({ guildID }, { notificationChannel: `${channel}` });
+      await interaction.reply(`:white_check_mark: Notification channel set to ${channel} successfully!`);
+
+    } else if (interaction.options.getSubcommand() === 'video_channel') {
+
+      const channel = interaction.options.getChannel('target');
+      await db.guild.findOneAndUpdate({ guildID }, { videoChannel: `${channel}` });
+      await interaction.reply(`:white_check_mark: Video Channel set to ${channel} successfully!`);
+
+    } else if (interaction.options.getSubcommand() === 'grace_period') {
+
+      const duration = interaction.options.getString('duration');
+      await db.guild.findOneAndUpdate({ guildID }, { videoGracePeriod: `${duration}` });
+      await interaction.reply(`:white_check_mark: Video Channel grace period duration set to ${duration} sec successfully!`);
+
+    } else if (interaction.options.getSubcommand() === 'auto_role') {
+
+      const role = interaction.options.getRole('target');
+      await db.guild.findOneAndUpdate({ guildID }, { autoRoles: `${role}` });
+      await interaction.reply(`:white_check_mark: Auto role set to ${role} successfully!`);
+
+    } else if (interaction.options.getSubcommand() === 'mod_role') {
+
+      const mod = interaction.options.getRole('target');
+      await db.guild.findOneAndUpdate({ guildID }, { modRoles: `${mod}` });
+      await interaction.reply(`:white_check_mark: Moderator role set to ${mod} successfully!`);
+
+    } else if (interaction.options.getSubcommand() === 'ban_role') {
+
+      const ban = interaction.options.getRole('target');
+      await db.guild.findOneAndUpdate({ guildID }, { banRoles: `${ban}` });
+      await interaction.reply(`:white_check_mark: Ban role set to ${ban} successfully!`);
+
+    }
+  }
+}
