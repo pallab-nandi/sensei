2d21aa960daea469e4e83f26a86ebe36bd58e71f
pallab-nandi <pallabnandi@outlook.in>
2023-06-23 16:11:00 +0530

    bug fix

--- commands/moderations/config.js
+++ commands/moderations/config.js
@@ -19,9 +19,9 @@ module.exports = {
       .setDescription("Modify any settings using: `/set <setting>` ")
       .setThumbnail("https://cdn-icons-png.flaticon.com/512/2385/2385865.png")
       .setFields(
-        { name: "Roles", value: `\`auto_role:\` ${guild.autoRoles === 'None' ? 'None' : `<@${guild.autoRoles}>`}\n\`mod_role:\` ${guild.modRoles === 'None' ? 'None' : `<@${guild.modRoles}>`}\n\`ban_role:\` ${guild.banRoles === 'None' ? 'None' : `<@${guild.banRoles}>`}` },
-        { name: "Video Channels", value: `\`video_channel:\` ${guild.videoChannel === 'None' ? 'None' : `<#${guild.videoChannel}>`}\n\`grace_period:\` ${guild.videoGracePeriod === 'None' ? 'None' : `${guild.videoGracePeriod}`}` },
-        { name: "Alert", value: `\`welcome_channel:\` ${guild.welcomeChannel === 'None' ? 'None' : `<#${guild.welcomeChannel}>`}\n\`notification_channel:\` ${guild.notificationChannel === 'None' ? 'None' : `<#${guild.notificationChannel}>`}` }
+        { name: "Roles", value: `\`auto_role:\` ${guild.autoRoles}\n\`mod_role:\` ${guild.modRoles}\n\`ban_role:\` ${guild.banRoles}` },
+        { name: "Video Channels", value: `\`video_channel:\` ${guild.videoChannel}\n\`grace_period:\` ${guild.videoGracePeriod}` },
+        { name: "Alert", value: `\`welcome_channel:\` ${guild.welcomeChannel}\n\`notification_channel:\` ${guild.notificationChannel}` }
       )
       .setImage("https://glitchii.github.io/embedbuilder/assets/media/banner.png")
       .setTimestamp();
